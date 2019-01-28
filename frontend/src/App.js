c2b00b78a3a224039ff94b8bb586c4802d03b203
Danielle Yang <dy6@illinois.edu>
2019-01-27 23:36:31 -0600

    Finished part 5!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import Instructions from './Instructions'
-import Counter from './Counter'
 import Show from './Show'
 
 class App extends Component {
@@ -10,8 +9,9 @@ class App extends Component {
       shows: [
         {id: 1, name: "Game of Thrones", episodes_seen: 0},
         {id: 2, name: "Naruto", episodes_seen: 220},
-        {id: 3, name: "Black Mirror", episodes_seen: 3},
-      ]
+        {id: 3, name: "Black Mirror", episodes_seen: 3}
+      ],
+      inputShowText: ""
     }
   }
 
@@ -19,13 +19,26 @@ class App extends Component {
     return this.props.complete
   };
 
+  handleChange = event => {
+    this.setState({inputShowText: event.target.value})
+  };
+
+  addShow = () => {
+    let newShow = this.state.shows
+    newShow.push({name: this.state.inputShowText, episodes_seen: 0, id: this.state.shows.length + 1})
+    this.setState({shows: newShow, inputShowText: ""})
+  };
+
   render() {
     return (
       <div className="App">
-        <Instructions/>
+        <Instructions complete = {false}/>
         {this.state.shows.map(x => (
           <Show id={x.id} name={x.name} episodes_seen={x.episodes_seen} />
         ))}
+        <p>{"Add a show: "}</p>
+        <input type = "text" value = {this.state.inputShowText} onChange = {this.handleChange}></input>
+        <button onClick={this.addShow}>Submit</button>
       </div>
     )
   }
